a2d52eef1f7d50245fe5f79d6fbb10da61735bfe
Clayton <clayton@claytonlz.com>
2012-11-29 11:17:05 -0700

    Test Template

---
+++ test/test_compound_interest.php
@@ -0,0 +1,21 @@
+<?php
+	
+	require_once("../src/compound_interest.php");
+	
+	/**
+	* TestCompoundInterest
+	*/
+	class CompoundInterestTest extends PHPUnit_Framework_TestCase
+	{
+		public function setUp()
+		{
+			
+		}
+		public function tearDown()
+		{
+			
+		}
+	}
+	
+
+?>
