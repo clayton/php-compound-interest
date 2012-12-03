5d63e5f26ff0dcf4e047d434a5f9af781717993c
Clayton <clayton@claytonlz.com>
2012-12-02 17:22:33 -0700

    TDD'd compound interest calculator

--- src/compound_interest.php
+++ src/compound_interest.php
@@ -1,3 +1,40 @@
 <?php
-
+	class CompoundInterest
+	{
+		
+		function CompoundInterest($principle, $term, $rate, $compoundings)
+		{
+			$this->principle = $principle;
+			$this->term = $term;
+			$this->rate = $rate;
+			$this->compoundings = $compoundings;
+		}
+		
+		public function calculate()
+		{
+			return $this->calculate_annual_compound_interest();
+		}
+		
+		private function rate_as_percentage()
+		{
+			return $this->rate / 100;
+		}
+		
+		private function exponent(){
+			return $this->term * $this->compoundings;
+		}
+		
+		private function rate_over_compoundings(){
+			return ($this->rate_as_percentage() / $this->compoundings);
+		}
+		
+		private function interest_calculation(){
+			return pow((1 + $this->rate_over_compoundings()), $this->exponent());
+		}
+		
+		private function calculate_annual_compound_interest(){
+			$raw = $this->principle * $this->interest_calculation();
+			return round($raw, 2);
+		}
+	}
 ?>
--- test/test_compound_interest.php
+++ test/test_compound_interest.php
@@ -1,19 +1,31 @@
 <?php
-	
-	require_once("../src/compound_interest.php");
-	
-	/**
-	* TestCompoundInterest
-	*/
+			
+	require_once(dirname(dirname(__FILE__))."/src/compound_interest.php");
+
 	class CompoundInterestTest extends PHPUnit_Framework_TestCase
 	{
-		public function setUp()
+		public function testSmallShortTermLowInterestQuarterlyCompoundedLoan()
 		{
-			
+			$calc = new CompoundInterest(100,2,3,4);
+			$this->assertEquals(106.16, $calc->calculate());
 		}
-		public function tearDown()
+		
+		public function testLargeShortTermLowInterestQuarterlyCompoundedLoan()
 		{
-			
+			$calc = new CompoundInterest(1000,2,3,4);
+			$this->assertEquals(1061.60, $calc->calculate());
+		}
+		
+		public function testLargeShortTermHighInterestQuarterlyCompoundedLoan()
+		{
+			$calc = new CompoundInterest(1000,2,50,4);
+			$this->assertEquals(2565.78, $calc->calculate());
+		}
+		
+		public function testLargeShortTermHighInterestMonthlyCompoundedLoan()
+		{
+			$calc = new CompoundInterest(1000,2,50,12);
+			$this->assertEquals(2663.73, $calc->calculate());
 		}
 	}
 	
